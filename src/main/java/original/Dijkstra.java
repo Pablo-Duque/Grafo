a913f3e6d8a2ff3412e13521c11f1187ca2feb3b
Pablo Duque <pabloduquesantos@gmail.com>
2024-06-19 23:24:55 -0300

    Tem um mundo

---
+++ src/main/java/original/Dijkstra.java
@@ -0,0 +1,115 @@
+package original;
+
+import grafo.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Dijkstra {
+
+		private int[][] matrizAdjacencia;
+		private String[] dadosVertices;
+		private int tamanho;
+                private int[] predecessores;
+
+		public Dijkstra(int tamanho) {
+			this.tamanho = tamanho;
+			this.matrizAdjacencia = new int[tamanho][tamanho];
+			this.dadosVertices = new String[tamanho];
+		}
+
+		public void adicionarAresta(int origem, int destino, int peso) {
+			if (verticeValido(origem) && verticeValido(destino)) {
+				matrizAdjacencia[origem][destino] = peso;
+				matrizAdjacencia[destino][origem] = peso;
+			}
+		}
+
+		public void adicionarDadosVertice(int vertice, String dados) {
+			if (verticeValido(vertice)) {
+				dadosVertices[vertice] = dados;
+			}
+		}
+
+		public int[] dijkstra(String dadosVerticeInicial) {
+			int verticeInicial = encontrarIndice(dadosVerticeInicial);
+			int[] distancias = new int[tamanho];
+			boolean[] visitados = new boolean[tamanho];
+                        predecessores = new int[tamanho];
+                        for (int i = 0; i < tamanho; i++) {
+                            predecessores[i] = -1;
+                        }
+                        
+			inicializarDistancias(distancias, verticeInicial);
+
+			for (int i = 0; i < tamanho; i++) {
+				int verticeAtual = encontrarMenorDistancia(distancias, visitados);
+				if (verticeAtual == -1)
+					break;
+				visitados[verticeAtual] = true;
+				atualizarDistancias(verticeAtual, distancias, visitados);
+			}
+			return distancias;
+		}
+
+		private boolean verticeValido(int vertice) {
+			return vertice >= 0 && vertice < tamanho;
+		}
+
+		private void inicializarDistancias(int[] distancias, int verticeInicial) {
+			for (int i = 0; i < tamanho; i++) {
+				distancias[i] = Integer.MAX_VALUE;
+			}
+			distancias[verticeInicial] = 0;
+		}
+
+		public int encontrarIndice(String dados) {
+			for (int i = 0; i < tamanho; i++) {
+				if (dadosVertices[i].equals(dados)) {
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		public int encontrarMenorDistancia(int[] distancias, boolean[] visitados) {
+			int menorDistancia = Integer.MAX_VALUE;
+			int indiceMenorDistancia = -1;
+
+			for (int i = 0; i < tamanho; i++) {
+				if (!visitados[i] && distancias[i] <= menorDistancia) {
+					menorDistancia = distancias[i];
+					indiceMenorDistancia = i;
+				}
+			}
+			return indiceMenorDistancia;
+		}
+
+		private void atualizarDistancias(int verticeAtual, int[] distancias, boolean[] visitados) {
+			for (int i = 0; i < tamanho; i++) {
+				if (!visitados[i] && matrizAdjacencia[verticeAtual][i] != 0
+						&& distancias[verticeAtual] != Integer.MAX_VALUE) {
+					int novaDistancia = distancias[verticeAtual] + matrizAdjacencia[verticeAtual][i];
+					if (novaDistancia < distancias[i]) {
+                                            distancias[i] = novaDistancia;
+                                            predecessores[i] = verticeAtual;
+                                        }
+				}
+			}
+		}
+                
+                public List<Integer> caminhoMaisCurto(int verticeDestino) {
+                    List<Integer> caminho = new ArrayList<>();
+                    caminho.add(verticeDestino);
+
+                    while (predecessores[verticeDestino] != -1) {
+                        verticeDestino = predecessores[verticeDestino];
+                        caminho.add(verticeDestino);
+                    }
+
+                    Collections.reverse(caminho);
+                    return caminho;
+                }
+
+	}
+
---
+++ src/main/java/original/Grafo.java
@@ -0,0 +1,90 @@
+package original;
+
+import javax.swing.*;
+import java.util.*;
+import java.util.List;
+
+public class Grafo extends JFrame {
+    private List<String> index = new ArrayList<String>();
+    private Dijkstra dijk = new Dijkstra(100);
+    
+    public Grafo(){
+        initializeGraph();
+    }
+    
+    private void initializeGraph() {
+        adicionarVertice("Sao Paulo");
+        adicionarVertice("Santos");
+        adicionarVertice("Sao Jose dos Campos");
+        adicionarVertice("Sorocaba");
+        adicionarVertice("Campinas");
+        adicionarVertice("Piracicaba");
+        adicionarVertice("Araraquara");
+        adicionarVertice("Riberao Preto");
+        adicionarVertice("Sao Jose do Rio Preto");
+        adicionarVertice("Bauru");
+        adicionarVertice("Marilia");
+        adicionarVertice("Presidente Prudente");
+        adicionarVertice("Araçatuba");
+        
+        adicionarAresta("Sao Paulo", "Santos", 85);
+        adicionarAresta("Sao Paulo", "Sao Jose dos Campos", 91);
+        adicionarAresta("Sao Paulo", "Sorocaba", 101);
+        adicionarAresta("Sao Paulo", "Campinas", 92);
+        adicionarAresta("Santos", "Sao Paulo", 85);
+        adicionarAresta("Sao Jose dos Campos", "Sao Paulo", 91);
+        adicionarAresta("Sorocaba", "Bauru", 244);
+        adicionarAresta("Sorocaba", "Presidente Prudente", 472);
+        adicionarAresta("Sorocaba", "Sao Paulo", 101);
+        adicionarAresta("Campinas", "Sao Paulo", 92);
+        adicionarAresta("Campinas", "Piracicaba", 72);
+        adicionarAresta("Campinas", "Araraquara", 185);
+        adicionarAresta("Campinas", "Riberao Preto", 222);
+        adicionarAresta("Piracicaba", "Campinas", 72);
+        adicionarAresta("Araraquara", "Campinas", 185);
+        adicionarAresta("Araraquara", "Sao Jose do Rio Preto", 168);
+        adicionarAresta("Riberao Preto", "Campinas", 222);
+        adicionarAresta("Sao Jose do Rio Preto", "Araraquara", 185);
+        adicionarAresta("Bauru", "Marilia", 106);
+        adicionarAresta("Bauru", "Sorocaba", 244);
+        adicionarAresta("Bauru", "Araçatuba", 191);
+        adicionarAresta("Marilia", "Bauru", 106);
+        adicionarAresta("Presidente Prudente", "Sorocaba", 472);
+        adicionarAresta("Araçatuba", "Bauru", 191);
+        
+        
+        System.out.println(acharMenorCaminho("Sao Paulo", "Bauru"));
+        List<Integer> caminho = new ArrayList<>();
+        caminho = dijk.caminhoMaisCurto(index.indexOf("Bauru"));
+        for(int a:caminho){
+            System.out.println(index.get(a) + "\n");
+        }
+ }
+    
+    public void adicionarVertice(String nome){
+        dijk.adicionarDadosVertice(index.size(), nome);
+        index.add(nome);
+    }
+    
+    public void adicionarAresta(String origemNome, String destinoNome, int distancia){
+        
+        if(index.contains(origemNome) && index.contains(destinoNome)){
+            int origem = index.indexOf(origemNome);
+            int destino = index.indexOf(destinoNome);
+            dijk.adicionarAresta(origem, destino, distancia);
+        }
+        else{
+            JOptionPane.showMessageDialog(this, "Vértice(s) inválido(s).");
+        }
+    }
+    
+    public int acharMenorCaminho(String origemNome, String destinoNome){
+        int [] distancias = dijk.dijkstra(origemNome);
+        return distancias[index.indexOf(destinoNome)];
+    }
+
+    public static void main(String[] args) {
+        new Grafo();
+    }
+    
+}
